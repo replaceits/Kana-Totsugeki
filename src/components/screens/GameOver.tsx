a905aea019abadd5117a9a31d8287bf0f55fb9f4
alic3 <alic3dev@gmail.com>
2023-11-15 18:09:08 -0500

    Add localization to `GameOver` /`MainMenu` screens

--- src/components/screens/GameOver.tsx
+++ src/components/screens/GameOver.tsx
@@ -1,23 +1,47 @@
+import React from 'react'
+
+import { ScreenName } from '.'
+import { Locale, LocalizationContext } from '../../contexts/localization'
+
 import './GameOver.scss'
 
-export default function GameOver() {
+export default function GameOver({
+  setCurrentScreen,
+}: {
+  setCurrentScreen: React.Dispatch<React.SetStateAction<ScreenName>>
+}) {
+  const localization = React.useContext<Locale>(LocalizationContext)
+
+  const onRestartClick = React.useCallback(() => {
+    setCurrentScreen('PlayField')
+  }, [setCurrentScreen])
+
+  const onMenuClick = React.useCallback(() => {
+    setCurrentScreen('MainMenu')
+  }, [setCurrentScreen])
+
   return (
     <div className="game-over">
       <div className="game-over-header">
-        <span className="score">Score</span>:{' '}
+        <span className="score">{localization.score}</span>:{' '}
         <span className="final-score mono">0</span>
       </div>
       <div className="game-over-header">
-        <span className="correct">Correct</span>:{' '}
+        <span className="correct">{localization.correct}</span>:{' '}
         <span className="final-correct mono">0</span>
       </div>
       <div className="game-over-header">
         <span className="seconds">Seconds</span>:{' '}
+        {/* FIXME: Add localization for this */}
         <span className="final-seconds mono">0</span>
       </div>
       <div className="game-over-buttons">
-        <div className="game-button restart">Restart</div>
-        <div className="game-button menu">Menu</div>
+        <div className="game-button" onClick={onRestartClick}>
+          {localization.restart}
+        </div>
+        <div className="game-button" onClick={onMenuClick}>
+          {localization.menu}
+        </div>
       </div>
     </div>
   )
--- src/components/screens/Main.tsx
+++ src/components/screens/Main.tsx
@@ -1,16 +1,40 @@
+import React from 'react'
+
+import { ScreenName } from '.'
+import { LocalizationContext } from '../../contexts/localization'
+
 import './Main.scss'
 
-export default function MainMenu() {
+export default function MainMenu({
+  setCurrentScreen,
+}: {
+  setCurrentScreen: React.Dispatch<React.SetStateAction<ScreenName>>
+}) {
+  const localization = React.useContext(LocalizationContext)
+
+  const onStartClick = React.useCallback(() => {
+    setCurrentScreen('PlayField')
+  }, [setCurrentScreen])
+
+  const onOptionsClick = React.useCallback(() => {
+    setCurrentScreen('Options')
+  }, [setCurrentScreen])
+
   return (
     <div className="game-main-menu">
       <div className="game-main-menu-header japanese">
-        仮名突撃
+        <h1>仮名突撃</h1>
+        <h5>かなとつげき</h5>
         <br />
-        Kana Assault
+        {/* Kana Assault */}
       </div>
       <div className="game-main-menu-buttons">
-        <div className="game-button start">Start</div>
-        <div className="game-button options">Options</div>
+        <div className="game-button start" onClick={onStartClick}>
+          {localization.start}
+        </div>
+        <div className="game-button options" onClick={onOptionsClick}>
+          {localization.options}
+        </div>
       </div>
     </div>
   )
