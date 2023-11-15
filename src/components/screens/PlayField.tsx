de8c7afa3ddb7138efe8090b66d1a444538db005
alic3 <alic3dev@gmail.com>
2023-11-15 18:46:25 -0500

    Preliminary game countdown timer

--- src/components/screens/PlayField.tsx
+++ src/components/screens/PlayField.tsx
@@ -1,11 +1,110 @@
+import React, { FormEvent } from 'react'
+
+import { ScreenName } from '.'
+
+import { SettingsContext, SettingsContextObject } from '../../contexts/settings'
+import { questions } from '../../data/questions'
+
 import './PlayField.scss'
 
-export default function PlayField() {
+export default function PlayField({
+  setCurrentScreen,
+}: {
+  setCurrentScreen: React.Dispatch<React.SetStateAction<ScreenName>>
+}) {
+  const settings = React.useContext<SettingsContextObject>(SettingsContext)
+  const validQuestions = React.useMemo<string[]>(() => {
+    const res = []
+
+    if (settings.settings.romajiQuestions) res.push(...Object.keys(questions))
+    if (settings.settings.hiraganaQuestions)
+      res.push(...Object.values(questions).map((q) => q.hiragana))
+    if (settings.settings.katakanaQuestions)
+      res.push(...Object.values(questions).map((q) => q.katakana))
+
+    return res
+  }, [
+    settings.settings.romajiQuestions,
+    settings.settings.hiraganaQuestions,
+    settings.settings.katakanaQuestions,
+  ])
+
+  const [currentQuestion, setCurrentQuestion] = React.useState<string>(
+    (): string =>
+      validQuestions[Math.floor(Math.random() * validQuestions.length)]
+  )
+  const [health, setHealth] = React.useState(settings.settings.lives)
+
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const countdownRef = React.useRef<HTMLDivElement>(null)
+
+  const resetGame = React.useCallback(() => {
+    inputRef.current?.focus()
+
+    setCurrentQuestion('')
+    setHealth(settings.settings.lives)
+  }, [settings, inputRef])
+
+  React.useEffect(() => {
+    inputRef.current?.focus()
+
+    let firstTime: number
+    let countdownAnimationFrameHandler: number
+
+    const countdownAnimationFrameRequest = (time: number): void => {
+      if (countdownRef.current === null) {
+        countdownAnimationFrameHandler = requestAnimationFrame(
+          countdownAnimationFrameRequest
+        )
+
+        return
+      }
+
+      if (!firstTime) firstTime = time
+      const elapsedTime: number = time - firstTime
+
+      // TODO: Determine animation length based upon settings + fail state on end
+      const completedPercentage =
+        Math.max((10000 - elapsedTime) / 10000, 0) * 100
+
+      countdownRef.current.style.width = `${completedPercentage}%`
+
+      if (completedPercentage) {
+        countdownAnimationFrameHandler = requestAnimationFrame(
+          countdownAnimationFrameRequest
+        )
+      }
+    }
+
+    countdownAnimationFrameHandler = requestAnimationFrame(
+      countdownAnimationFrameRequest
+    )
+
+    return () => cancelAnimationFrame(countdownAnimationFrameHandler)
+  }, [])
+
+  React.useEffect(() => {
+    if (health === 0) {
+      setCurrentScreen('GameOver')
+    }
+  }, [health, setCurrentScreen])
+
+  const onAnswerSubmit = React.useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key !== 'Enter' && event.code !== 'Enter') return
+
+      // TODO: Check answer
+
+      alert(event.currentTarget.value)
+    },
+    []
+  )
+
   return (
     <div className="game-play-field-wrapper">
-      <div className="game-countdown-timer"></div>
+      <div className="game-countdown-timer" ref={countdownRef}></div>
       <div className="game-play-field">
-        <div className="game-question japanese"></div>
+        <div className="game-question japanese">{currentQuestion}</div>
         <div className="game-input">
           <div className="carrot mono">&gt;</div>
           <input
@@ -13,27 +112,38 @@ export default function PlayField() {
             className="game-input-answer"
             maxLength={3}
             autoFocus
+            autoComplete="off"
+            onKeyUp={onAnswerSubmit}
+            ref={inputRef}
           />
         </div>
         <div className="game-input-underline"></div>
         <div className="game-score mono">0</div>
       </div>
       <div className="game-heart-container">
-        <div className="game-heart">
+        <div className={`game-heart ${health < 1 ? 'down' : ''}`}>
           <i className="fa fa-heart" aria-hidden="true"></i>
         </div>
-        <div className="game-heart">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="game-heart">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="game-heart">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
-        <div className="game-heart">
-          <i className="fa fa-heart" aria-hidden="true"></i>
-        </div>
+        {settings.settings.lives > 1 && (
+          <div className={`game-heart ${health < 2 ? 'down' : ''}`}>
+            <i className="fa fa-heart" aria-hidden="true"></i>
+          </div>
+        )}
+        {settings.settings.lives > 2 && (
+          <div className={`game-heart ${health < 3 ? 'down' : ''}`}>
+            <i className="fa fa-heart" aria-hidden="true"></i>
+          </div>
+        )}
+        {settings.settings.lives > 3 && (
+          <div className={`game-heart ${health < 4 ? 'down' : ''}`}>
+            <i className="fa fa-heart" aria-hidden="true"></i>
+          </div>
+        )}
+        {settings.settings.lives > 4 && (
+          <div className={`game-heart ${health > 5 && 'down'}`}>
+            <i className="fa fa-heart" aria-hidden="true"></i>
+          </div>
+        )}
       </div>
     </div>
   )
