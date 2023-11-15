0bb9fe3f10e25b3e752db2f29a57c7e1c3879668
alic3 <alic3dev@gmail.com>
2023-11-15 18:06:02 -0500

    Add settings context and data

---
+++ src/contexts/settings.ts
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import { defaultSettings, Setting, Settings } from '../data/settings'
+
+interface SettingsDispatchAction {
+  type: 'update' | 'toggle'
+  name: keyof Settings
+  value?: Setting
+}
+
+type SettingsDispatch = React.Dispatch<SettingsDispatchAction>
+
+export interface SettingsContextObject {
+  settings: Settings
+  settingsDispatch: SettingsDispatch
+}
+
+export const SettingsContext = React.createContext<SettingsContextObject>({
+  settings: defaultSettings,
+  settingsDispatch: () => {},
+})
+
+const settingsReducer = (
+  state: Settings,
+  action: SettingsDispatchAction
+): Settings => {
+  if (action.type === 'update') {
+    return { ...state, [action.name]: action.value }
+  }
+
+  if (action.type === 'toggle') {
+    if (action.name === 'language') {
+      return {
+        ...state,
+        language: state.language === 'japanese' ? 'english' : 'japanese',
+      }
+    }
+
+    return { ...state, [action.name]: !state[action.name] }
+  }
+
+  return state
+}
+
+export const useSettingsReducer = (): [Settings, SettingsDispatch] => {
+  const [settings, settingsDispatch] = React.useReducer(
+    settingsReducer,
+    defaultSettings,
+    (defaultSettings) => {
+      try {
+        return {
+          ...defaultSettings,
+          ...(JSON.parse(
+            window.localStorage.getItem('kana-totsugeki:settings') || '{}'
+          ) as Settings),
+        }
+      } catch {
+        window.localStorage.removeItem('kana-totsugeki:settings')
+      }
+
+      return defaultSettings
+    }
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(
+      'kana-totsugeki:settings',
+      JSON.stringify(settings)
+    )
+  }, [settings])
+
+  return [settings, settingsDispatch]
+}
+
+export default SettingsContext
---
+++ src/data/settings.ts
@@ -0,0 +1,34 @@
+export type LanguageSetting = 'english' | 'japanese'
+export type SpeedSetting = 'slow' | 'medium' | 'fast'
+
+export type Setting = boolean | number | LanguageSetting | SpeedSetting
+
+export interface Settings {
+  hiraganaQuestions: boolean
+  katakanaQuestions: boolean
+  romajiQuestions: boolean
+  hiraganaAnswers: boolean
+  katakanaAnswers: boolean
+  romajiAnswers: boolean
+  noRepeats: boolean
+  showCorrect: boolean
+  lives: number
+  language: LanguageSetting
+  speed: SpeedSetting
+}
+
+export const defaultSettings: Settings = {
+  hiraganaQuestions: true,
+  katakanaQuestions: true,
+  romajiQuestions: false,
+  hiraganaAnswers: true,
+  katakanaAnswers: true,
+  romajiAnswers: true,
+  noRepeats: true,
+  showCorrect: true,
+  lives: 3,
+  language: 'english',
+  speed: 'medium',
+}
+
+export default defaultSettings
